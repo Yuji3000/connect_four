0290c01821bf36a648e34fd237573dd154fdafa6
Krisetn Nestler <kristen.nestler@gmail.com>
2022-09-01 15:27:04 -0400

    feat: add board render and move methods and tests

--- lib/board.rb
+++ lib/board.rb
@@ -1,20 +1,41 @@
 require 'pry'
 class Board
+
+attr_reader :cells
+  update
   def initialize
-    @board = grid
-  end
-
-
-  def grid
-    puts ["ABCDEFG"]
-    @cells = {}   
-    (1..6).each do |i|
-      ('a'..'g').each do |letter|
-        @cells["#{letter}#{i}"] = "."
-          # binding.pry
-          print @cells["#{letter}#{i}"]
-        end
-      puts
+    @numbers = (1..6).to_a
+    @letters = ('a'..'g').to_a
+    @cells = {}
+    board_cells
+  end 
+  
+  def board_cells
+    @numbers.each do |number|
+      @letters.each do |letter|
+        @cells["#{letter}#{number}"] = ('.')
       end
+    end.reverse
   end
+  
+  def render_board
+    render = "ABCBEFG\n" +
+    @cells["a1"] + @cells["b1"] + @cells["c1"] + @cells["d1"] + @cells["e1"] + @cells["f1"] + @cells["g1"] + " \n" +
+    @cells["a2"] + @cells["b2"] + @cells["c2"] + @cells["d2"] + @cells["e2"] + @cells["f2"] + @cells["g2"] + " \n" +
+    @cells["a3"] + @cells["b3"] + @cells["c3"] + @cells["d3"] + @cells["e3"] + @cells["f3"] + @cells["g3"] + " \n" +
+    @cells["a4"] + @cells["b4"] + @cells["c4"] + @cells["d4"] + @cells["e4"] + @cells["f4"] + @cells["g4"] + " \n" +
+    @cells["a5"] + @cells["b5"] + @cells["c5"] + @cells["d5"] + @cells["e5"] + @cells["f5"] + @cells["g5"] + " \n" +
+    @cells["a6"] + @cells["b6"] + @cells["c6"] + @cells["d6"] + @cells["e6"] + @cells["f6"] + @cells["g6"]
+    print render + "\n"
+  end
+  
+  def render_move(given)
+    cell_arr = @cells.sort.reverse
+    cell_arr.find do |cell| 
+        cell[1] = 'X' if cell[0].include?(given) && cell[1] == '.'
+      end
+    @cells = cell_arr.to_h
+  end
+  
+
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -9,8 +9,38 @@ RSpec.describe Board do
     end
   end
 
-  # it 'test the grid' do
-  #   board = Board.new
-  #   expect(board.grid).to be_a_kind_of Hash
-  # end
+  it 'can build cells' do 
+      board = Board.new
+      # board.build_board
+      expect(board.cells.values[0]).to eq(".") 
+      expect(board.cells.values[41]).to eq(".") 
+      expect(board.cells.values[42]).to eq(nil) 
+      # expect(board.build_board.values[0]).to eq(["a1", "b1", "c1", "d1", "e1", "f1", "g1"])
+      # expect(board.build_board.keys[1]).to eq(["a2", "b2", "c2", "d2", "e2", "f2", "g2"])
+      # expect(board.build_board.keys[2]).to eq(["a3", "b3", "c3", "d3", "e3", "f3", "g3"])
+    end
+    
+    it 'can render a new board' do 
+      board = Board.new
+      # player = Player.new
+      expect(board.render_board).to eq(".")
+      # expect(board.render_board.values[1]).to eq[[".", ".", ".", ".", ".", ".", "."]]
+      # expect(board.render_board.values[2]).to eq[[".", ".", ".", ".", ".", ".", "."]]
+      #   board.move("a")
+      # expect(board.render_board.values[5]).to eq[["X", ".", ".", ".", ".", ".", "."]]
+      #   board.move("d")
+      # expect(board.render_board.values[5]).to eq[["X", ".", ".", "X", ".", ".", "."]]
+    end
+
+  ## better in game class?
+    it 'can change key values based on player input' do 
+      board = Board.new
+      # expect(board.render_board).to eq()
+      expect(board.move("a")).to be_a Hash
+      expect(board.move("a").count).to eq 42
+      expect(board.move("a")).to include("a1" => ".")
+      expect(board.move("a")).to include("a2" => ".")
+      expect(board.move("a")).to include("a3" => "X")
+    end
+    
 end
